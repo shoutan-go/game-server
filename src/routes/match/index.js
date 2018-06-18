d58bf1e934aff46378c15793a24ad65234e1f9b4
Jingsi <lookisliu@gmail.com>
2018-06-18 08:53:51 +0800

    fix async bug for return

--- src/routes/match/index.js
+++ src/routes/match/index.js
@@ -5,32 +5,34 @@ import React from 'react';
 import swal from 'sweetalert2';
 import queryMatch from './match.graphql';
 
-async function action({ query: { id }, client }) {
-  const { data } = await client.query({
-    query: queryMatch,
-    variables: {
-      id,
-    },
-  });
-
-  if (data && data.match && data.match.id) {
-    return {
-      redirect: `/go?id=${data.match.id}&invite=${data.match.color}`,
-    };
-  }
-  if (typeof window !== 'undefined') {
-    swal({
-      type: 'error',
-      title: '比赛已结束',
-    }).then(() => {
-      wx.closeWindow();
+function action({ query: { id }, client }) {
+  return client
+    .query({
+      query: queryMatch,
+      variables: {
+        id,
+      },
+    })
+    .then(({ data }) => {
+      if (data && data.match && data.match.id) {
+        return {
+          redirect: `/go?id=${data.match.id}&invite=${data.match.color}`,
+        };
+      }
+      if (typeof window !== 'undefined') {
+        swal({
+          type: 'error',
+          title: '比赛已结束',
+        }).then(() => {
+          wx.closeWindow();
+        });
+      }
+      return {
+        chunks: ['match'],
+        title: '匹配中',
+        component: <div />,
+      };
     });
-  }
-  return {
-    chunks: ['match'],
-    title: '匹配中',
-    component: <div />,
-  };
 }
 
 export default action;
